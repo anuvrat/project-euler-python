043bac80b8f30a6746054021ee4f8761026827fa
Anuvrat Singh <singh.anuvrat@gmail.com>
2012-09-08 17:25:20 +0530

    A much faster partition function implementation using pentagonal numbers. Solves Project Ruler problem 78 in half a minute and problem 76 in less than a second.

--- src/problems_71_80/problem_78.py
+++ src/problems_71_80/problem_78.py
@@ -9,26 +9,37 @@ class Memoize:
         self.cache = {}
 
     def __call__( self, *args ):
-        if not args in self.cache:
-            self.cache[args] = self.f( *args )
+        if not args in self.cache: self.cache[args] = self.f( *args )
         return self.cache[args]
 
 
 @Memoize
-def sigma_1( n ):
-    return sum( filter( lambda k: n % k == 0, range( 1, n + 1 ) ) )
+def sigma_1( n ): return sum( filter( lambda k: n % k == 0, range( 1, n + 1 ) ) )
 
 @Memoize
 def partitions( n ):
-    if n in range( 11 ):
-        return ( 1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42 )[n]
-    else:
-        return sum( sigma_1( n - k ) * partitions( k ) for k in range( n ) ) // n
+    if n in range( 11 ): return ( 1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42 )[n]
+    else: return sum( sigma_1( n - k ) * partitions( k ) for k in range( n ) ) // n
 
-idx = 12
-val = partitions( idx )
-while not val % 10000 == 0:
-    idx += 1
-    val = partitions( idx )
+@Memoize
+def partitions_faster( n ):
+    if n in range( 11 ): return ( 1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42 )[n]
 
-print( "For idx " + str( idx ) + " value = " + str( val ) )
+    total = 0
+    for k in range( 1, n + 1 ):
+        if n - ( k * ( 3 * k - 1 ) // 2 ) < 0: break
+        total += pow( -1, k + 1 ) * ( partitions_faster( n - ( k * ( 3 * k - 1 ) // 2 ) ) + partitions_faster( n - ( k * ( 3 * k + 1 ) // 2 ) ) )
+
+    return total
+
+if __name__ == '__main__':
+    print( partitions_faster( 100 ) )
+
+    idx = 12
+    val = partitions_faster( idx )
+    while not val % 1000000 == 0:
+        idx += 1
+        val = partitions_faster( idx )
+        print( "For idx " + str( idx ) + " value = " + str( val ) )
+
+    print( "For idx " + str( idx ) + " value = " + str( val ) )
