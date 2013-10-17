24b45bcb59f045eddde4bf2adddf3b477bd968d1
Anuvrat Singh <anuvrat@amazon.com>
2013-10-17 15:50:17 +0530

    Solved problem 73

---
+++ src/problem_71_80/problem_73.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 17, 2013
+
+@author: anuvrat
+'''
+from fractions import gcd
+
+def getCountOfFractions():
+    return sum( 1 for d in range( 5, 12001 ) for n in range( int( d / 3 ) + 1, int( d / 2 ) + 1 ) if gcd( n, d ) == 1 )
+
+if __name__ == '__main__':
+    print( getCountOfFractions() )
